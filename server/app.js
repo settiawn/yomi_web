4481fc89645b88177bfef1f4dba4bfb0719a81c1
Aryo Baskoro Setiawan <aryobaskorosetiawan@gmail.com>
2024-02-12 23:44:13 +0700

    chore: add bcrypt helper

--- server/app.js
+++ server/app.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
