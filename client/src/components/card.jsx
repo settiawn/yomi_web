1acc8bc0ee6eaae6a44726119ebed492fe656d30
Aryo Baskoro Setiawan <aryobaskorosetiawan@gmail.com>
2024-02-14 23:11:17 +0700

    fix: search and read feature

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Swal from 'sweetalert2'
 
 export function Card(params) {
   const { id, name, cover } = params;
   const [image, setImage] = useState("https://corona.s-ul.eu/P2xNVZkN.jpg");
+  const [link, setLink] = useState("");
 
   useEffect(() => {
     getImageLink();
@@ -16,10 +19,36 @@ export function Card(params) {
         url: "https://api.mangadex.org/cover/" + cover,
       });
 
-      let a = `https://uploads.mangadex.org/covers/${id}/${data.data.attributes.fileName}`;
-      setImage(a);
+      let img = `https://uploads.mangadex.org/covers/${id}/${data.data.attributes.fileName}`;
+      let link = `https://mangadex.org/title/${id}`;
+      setImage(img);
+      setLink(link);
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  async function addToList() {
+    try {
+      const {data} = await axios({
+        method: "post",
+        url: "http://localhost:3000/index/" + id,
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("access_token"),
+        },
+      });
+      Swal.fire({
+        title: "Success",
+        text: data.message,
+        icon: "success"
+      });
+    } catch (error) {
+      console.log(error);
+      Swal.fire({
+        title: "Oops...",
+        text: error.response.data.message,
+        icon: "error"
+      });
     }
   }
   //   max-w-sm
@@ -34,10 +63,17 @@ export function Card(params) {
         </div>
       </div>
       <div className="flex justify-between absolute bottom-0 w-full">
-        <div className="w-1/2 bg-green-500 font-bold p-3 text-white hover:cursor-pointer">
+        <Link
+          to={link}
+          target="_blank"
+          className="w-1/2 bg-green-500 font-bold p-3 text-white hover:cursor-pointer"
+        >
           Read
-        </div>
-        <div className="w-1/2 bg-yellow-500 font-bold p-3 text-white hover:cursor-pointer">
+        </Link>
+        <div
+          className="w-1/2 bg-yellow-500 font-bold p-3 text-white hover:cursor-pointer"
+          onClick={addToList}
+        >
           Add
         </div>
       </div>
