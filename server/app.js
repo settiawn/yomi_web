6d02623fbfcb03f1a8c5d8e31acc7ae3313b694a
Aryo Baskoro Setiawan <aryobaskorosetiawan@gmail.com>
2024-02-16 06:52:35 +0700

    fix: setup for deploy

--- server/app.js
+++ server/app.js
@@ -1,20 +1,23 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const { errorHandler } = require("./middlewares/errorhandler");
 const router = require("./routes");
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
-app.use(router)
+app.use(router);
 app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-module.exports = app
+module.exports = app;
